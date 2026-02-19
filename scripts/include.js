c183e6b9141e5cc8e91f7ecffdd65cab3483476c
HedonisticOpportunist <fujitahibaraki@gmail.com>
2026-02-19 02:19:21 +0100

    Update include.js

--- scripts/include.js
+++ scripts/include.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Inject header
   const header = document.getElementById("header");
   if (header) {
-    fetch("../header/header.html")
+    fetch("header/header.html")
       .then((res) => res.text())
       .then((html) => {
         header.innerHTML = html;
@@ -12,12 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
   // Inject footer
   const footer = document.getElementById("footer");
   if (footer) {
-    fetch("../footer/footer.html")
+    fetch("footer/footer.html")
       .then((res) => res.text())
       .then((html) => {
         footer.innerHTML = html;
         const script = document.createElement("script");
-        script.src = "../scripts/footer.js";
+        script.src = "scripts/footer.js";
         document.body.appendChild(script);
       });
   }
